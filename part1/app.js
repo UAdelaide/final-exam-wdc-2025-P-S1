b835eec49ef4f3b97f367b8f1955c72b887356ef
P-S1 <pranav.singh232@gmail.com>
2025-06-20 03:41:55 +0000

    [automatic commit] 2025-06-20 03:41:55 UTC 129.127.202.8

--- part1/app.js
+++ part1/app.js
@@ -1,7 +1,7 @@
 
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        
+
         const sqlQuery = `
             SELECT
                 walkReq.request_id,
@@ -17,17 +17,17 @@ app.get('/api/walkrequests/open', async (req, res) => {
             ORDER BY walkReq.requested_time ASC, dogInfo.name ASC
         `;
 
-        // Execute database query and destructure results
+
         const [queryResults] = await db.execute(sqlQuery);
 
-        // Send successful response with open walk requests data
+
         res.status(200).json(queryResults);
 
     } catch (dbError) {
-        // Log error details for debugging purposes
+
         console.error('Database query failed for open walk requests:', dbError);
 
-        // Return structured error response to client
+        
         res.status(500).json({
             error: 'Database operation failed',
             message: 'Unable to fetch open walk requests at this time',
