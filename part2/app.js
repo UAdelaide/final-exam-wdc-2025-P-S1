254264a49321281fab25dff47bdd60e708577c8b
P-S1 <pranav.singh232@gmail.com>
2025-06-20 06:02:42 +0000

    [automatic commit] 2025-06-20 06:02:41 UTC 129.127.202.8

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // Set to true in production with HTTPS
+    secure: false, 
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all walk requests
+// get all walk requests
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
